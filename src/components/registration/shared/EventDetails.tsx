dbfe61c3f2d917b261859c93354c41fc87d6d2c8
DIPSHA <dipshadas4@gmail.com>
2025-10-18 10:05:54 +0530

    Made the tag line mobile responsive

--- src/components/registration/shared/EventDetails.tsx
+++ src/components/registration/shared/EventDetails.tsx
@@ -7,7 +7,7 @@ interface EventDetailsProps {
 export default function EventDetails({ event }: EventDetailsProps) {
   return (
     <div className="bg-yellow-50 p-6 border-b border-yellow-200">
-      <p className="text-gray-950 text-2xl font-bold mb-4">{event.description}</p>
+      <p className="text-gray-950 text-lg sm:text-xl md:text-2xl font-bold  mb-4">{event.description}</p>
       
       <div className="space-y-1 text-gray-800 text-xl">
         <p><strong>Date:</strong> {event.date}</p>
